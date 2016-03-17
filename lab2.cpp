880d91b3f8f3132e3a0ce3ef91c99aaef28fca1f
smi97 <smi97@mail.ru>
2016-03-17 18:34:05 +0300

    Added files via upload

---
+++ lab2.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string pass = "main.cpp";
+    ifstream file;
+    file.open(pass);
+    if (!file.is_open()){
+        cout << "Ошибка открытия файла!\n";
+        return 1;
+    }
+
+    int i = 0, j = 0, k = 0;
+    string str;
+    size_t n;
+    bool f = false;
+
+    while (!file.eof()) {
+        getline(file, str);
+        n = str.find("/**");
+        if(n != string::npos){
+            n = str.find("\\func");
+            k = n + 6;
+            if (n != string::npos) {
+                j++;
+                cout << j << ". ";
+                while (str[str.find("*/")] != str[k]){
+                cout<< str[k];
+                    k++;
+                }
+                cout << endl;
+            }
+            else {
+                do {
+                    getline(file, str);
+                    n = str.find("\\func");
+                    k = n + 6;
+                    if(n != string::npos)
+                    {
+                        f = true;
+                        j++;
+                        cout << j << ". ";
+                        while (str[str.find("*/")] != str[k]){
+                            cout<< str[k];
+                            k++;
+                        }
+                        cout << endl;
+
+                    }
+                    if(file.eof()) break;
+                }
+                while (!f);
+            }
+            f = false;
+        }
+    }
+
+
+    file.close();
+
+    return 0;
+}
+
