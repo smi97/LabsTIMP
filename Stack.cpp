f4b60319d58b61fdee6b680f6f8374ce9ee5d7ab
smi97 <smi97@mail.ru>
2016-03-30 11:43:33 +0300

    Added files via upload

---
+++ Stack.cpp
@@ -0,0 +1,101 @@
+#include <cstring>
+#include <string>
+#include <iostream>
+
+
+typedef struct Stack
+{
+    int el;
+    Stack *head, *next;
+} Stack;
+
+void push(int iEl, Stack *stack)
+{
+    Stack *tmp = new Stack;
+    tmp->el = iEl;
+    tmp->next=stack->head;
+    stack->head=tmp;
+}
+
+int get(Stack *stack){
+    return stack->head->el;
+}
+
+int pop(int *iEl, Stack *stack)
+{
+    if(stack->head == nullptr) return 0;
+    Stack *tmp;
+    tmp = stack->head;
+    *iEl = tmp->el;
+    stack->head = tmp->next;
+    free(tmp);
+    return 1;
+}
+
+struct Stack * CreateStack()
+{
+    struct Stack *stack = new Stack;
+    stack->head = nullptr;
+    stack->next = nullptr;
+    return stack;
+}
+
+int isStackEmpty(Stack *stack)
+{
+    if(stack->head == nullptr) return 1;
+    return 0;
+}
+
+int recognize(std::string com, int & data)
+{
+    if ( com.find("add") != std::string::npos ) {data = std::stoi(com.substr(com.find("add") + 4, 100)); return 1;}
+    if ( com.find("get") != std::string::npos )  return 2;
+    if ( com.find("del") != std::string::npos )  return 3;   return 0;
+}
+
+
+int main()
+{
+    int i, n = 0, c, d, a, data;
+    std::string command;
+    Stack *q = CreateStack();
+    std::cin >> c;
+    getline(std::cin, command);
+    for(i = 0; i < c; i++){
+        getline(std::cin, command);
+        a = recognize(command, data);
+        switch(a)
+        {
+            case 1:
+                push(data, q);
+                std::cout << "\nЭлемент добавлен в стек \n";
+                break;
+
+
+            case 2:
+                if (isStackEmpty(q)==1)
+                    std::cout << "\nСтек пуст \n";
+                else
+                {
+                    std::cout << get(q) << std::endl;
+                }
+                break;
+
+            case 3:
+                if (isStackEmpty(q) == 1)
+                    std::cout << "\nСтек пуст \n";
+                else
+                {
+                    pop(&i, q);
+                    std::cout << "\nЭлемент удален из стека \n";
+                }
+                break;
+
+            default: std::cout << "\nОшибка! Неправильно введена команда! \n"; break;
+        }
+    }
+
+    return 0;
+}
+
+
